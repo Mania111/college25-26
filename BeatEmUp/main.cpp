ccaaddf5f840f16d2e8144b873d84d2aee8e8c52
Mania111 <124902290+Mania111@users.noreply.github.com>
2026-01-18 19:48:08 +0100

    Implement jump and attack actions with sprites
    Added jump and attack sprites, updated player action handling.

--- BeatEmUp/main.cpp
+++ BeatEmUp/main.cpp
@@ -81,6 +81,8 @@ void NewGame(double &stageTime, double &cameraX, double &cameraY, double &player
 	playerY = 350.0;
 }
 
+struct RectD { double x, y, w, h; };
+
 // main
 #ifdef __cplusplus
 extern "C"
@@ -93,6 +95,7 @@ int main(int argc, char **argv) {
 	SDL_Event event;
 	SDL_Surface *screen, *charset;
 	SDL_Surface *sprStand, *sprWalk1, *sprWalk2;
+	SDL_Surface *sprJump, sprAttackHeavy, *sprAttackLight;
 	SDL_Texture *scrtex;
 	SDL_Window *window;
 	SDL_Renderer *renderer;
@@ -153,12 +156,17 @@ int main(int argc, char **argv) {
 	sprStand = SDL_LoadBMP("./player_stand.bmp");
 	sprWalk1 = SDL_LoadBMP("./player_walk1.bmp");
 	sprWalk2 = SDL_LoadBMP("./player_walk2.bmp");
+	sprJump = SDL_LoadBMP("./player_jump.bmp");
+	sprAttackHeavy = SDL_LoadBMP("./player_attack.bmp");
+	sprAttackLight = SDL_LoadBMP("./player_attack2.bmp");
 
-	if(!sprStand || !sprWalk1 || !sprWalk2) {
+	if(!sprStand || !sprWalk1 || !sprWalk2 || !sprJump || !sprAttackHeavy || !sprAttackLight) {
 		printf("SDL_LoadBMP(player sprites) error: %s\n", SDL_GetError());
 		if (sprStand) SDL_FreeSurface(sprStand);
 		if (sprWalk1) SDL_FreeSurface(sprWalk1);
 		if (sprWalk2) SDL_FreeSurface(sprWalk2);
+		if (sprAttackHeavy) SDL_FreeSurface(sprAttackHeavy);
+		if (sprAttackLight) SDL_FreeSurface(sprAttackLight);
 		SDL_FreeSurface(charset);
 		SDL_FreeSurface(screen);
 		SDL_DestroyTexture(scrtex);
@@ -172,6 +180,9 @@ int main(int argc, char **argv) {
 	SDL_SetColorKey(sprStand, SDL_TRUE, key);
 	SDL_SetColorKey(sprWalk1, SDL_TRUE, key);
 	SDL_SetColorKey(sprWalk2, SDL_TRUE, key);
+	SDL_SetColorKey(sprJump, SDL_TRUE, key);
+	SDL_SetColorKey(sprAttackHeavy, SDL_TRUE, key);
+	SDL_SetColorKey(sprAttackLight, SDL_TRUE, key);
 
 	char text[128];
 	int czarny = SDL_MapRGB(screen->format, 0x00, 0x00, 0x00);
@@ -197,11 +208,31 @@ int main(int argc, char **argv) {
 	const int FLOOR_Y = 260;
 	const int FLOOR_H = 200;
 
+	// player variables
 	double playerX = 200.0;
 	double playerY = FLOOR_Y + FLOOR_H / 2.0;
 
 	double playerSpeed = 260.0;
 
+	// for action
+	double gravity = 2200.0;
+	double jumpVel = 950.0; // bigger = higher jump
+	double moveSpeed = 260.0; // same as player speed
+
+	double lightAttackDuration = 0.18; // seconds
+	double heavyAttackDuration = 0.35;
+	double attackMoveScale = 0.35; // how much you can move during an attack
+
+	// action state
+	bool inJump = false;
+	double z = 0.0; // vertical offset - how high above the ground ( purely visual )
+	double vz = 0.0; // vertical velocity
+
+	enum Action { ACT_NONE, ACT_LIGHT, ACT_HEAVY };
+	Action action = ACT_NONE;
+	double actionTimer = 0.0;
+
+	// camera variables
 	double cameraX = 0.0;
 	double cameraY = 0.0;
 	// camera dead-zone
@@ -230,9 +261,31 @@ int main(int argc, char **argv) {
 
 		distance += etiSpeed * delta;
 
+		if (inJump) {
+			vz -= gravity * delta; // gravity pulls down
+			z += vz * delta;
+
+			if (z <= 0.0) {
+				z = 0.0;
+				vz = 0.0;
+				inJump = false;
+			}
+		}
+
+		if (inAttack) {
+			attackTimer -= delta;
+			if (attackTimer <= 0.0) {
+				inAttack = false;
+				attackTimer = 0.0;
+			}
+		}
+
 		const Uint8 *keys = SDL_GetKeyboardState(NULL);
 
 		double vx = 0.0, vy = 0.0;
+
+		double speedScale = 1.0;
+		if (inAttack) speedScale = attackMoveScale;
 
 		if(keys[SDL_SCANCODE_LEFT] || keys[SDL_SCANCODE_A]) vx -= 1.0;
 		if(keys[SDL_SCANCODE_RIGHT] || keys[SDL_SCANCODE_D]) vx += 1.0;
@@ -245,6 +298,9 @@ int main(int argc, char **argv) {
 			vx /= len;
 			vy /= len;
 		}
+
+		playerX += vx * playerSpeed * speedScale * delta;
+		playerY += vy * playerSpeed * speedScale * delta;
 
 		bool moving = (len > 0.0);
 		if (moving) {
@@ -259,12 +315,37 @@ int main(int argc, char **argv) {
 		}
 
 		SDL_Surface* currentSprite = sprStand;
-		if (moving) {
-			if (animStep == 1) currentSprite = sprWalk1;
-			else if (animStep == 3) currentSprite = sprWalk2;
-			else currentSprite = sprStand;
-		} else {
-			currentSprite = sprStand;
+
+		if (inAttack) {
+			currentSprite = sprAttack;
+		}
+		else if (inJump) {
+			currentSprite = sprJump;
+		}
+		else {
+			if (moving) {
+				if (animStep == 1) currentSprite = sprWalk1;
+				else if (animStep == 3) currentSprite = sprWalk2;
+				else currentSprite = sprStand;
+			} else {
+				currentSprite = sprStand;
+			}
+		}
+
+		RectD attackbox = {0, 0, 0, 0};
+		bool attackActive = false;
+
+		if (inAttack) {
+			attackActive = true;
+
+			// forward hitbox
+			double range = 90.0;
+			double height = 80.0;
+
+			attackBox.w = range;
+			attackBox.h = height;
+			attackBox.x = playerX + 60.0;
+			attackBox.y = playerY - height;
 		}
 
 		playerX += vx * playerSpeed * delta;
@@ -275,8 +356,8 @@ int main(int argc, char **argv) {
 		if (playerX > STAGE_W) playerX = STAGE_W;
 
 		// floor lane clamp
-		double footTop = FLOOR_Y + 15; // top of lane
-		double footBottom = FLOOR_Y + FLOOR_H - 5; // bottom of lane
+		double footTop = FLOOR_Y + 30; // top of lane
+		double footBottom = FLOOR_Y + FLOOR_H; // bottom of lane
 		if (playerY < footTop) playerY = footTop;
 		if (playerY > footBottom) playerY = footBottom;
 
@@ -312,10 +393,10 @@ int main(int argc, char **argv) {
 		if (cameraX > STAGE_W - SCREEN_WIDTH) cameraX = STAGE_W - SCREEN_WIDTH;
 
 		// background
-		int sky = SDL_MapRGB(screen->format, 30, 30, 60);
-		int bg2 = SDL_MapRGB(screen->format, 20, 20, 40);
-		int floorCol = SDL_MapRGB(screen->format, 60, 60, 60);
-		int floorEdge = SDL_MapRGB(screen->format, 120, 120, 120);
+		int sky = SDL_MapRGB(screen->format, 25, 25, 55);
+		int bg2 = SDL_MapRGB(screen->format, 150, 125, 65);
+		int floorCol = SDL_MapRGB(screen->format, 85, 95, 85);
+		int floorEdge = SDL_MapRGB(screen->format, 25, 25, 30);
 
 		SDL_FillRect(screen, NULL, sky);
 
@@ -324,22 +405,21 @@ int main(int argc, char **argv) {
 			int x = i - (int)(cameraX * 0.2) % 80;
 			DrawRectangle(screen, x, 60, 40, 80, bg2, bg2);
 		}
-
-		// floor
-		int floorScreenY = FLOOR_Y - (int)cameraY;
-		DrawRectangle(screen, 0, floorScreenY, SCREEN_WIDTH, FLOOR_H, floorEdge, floorCol);
-
 		// stage limit markers
-		int markerCol = SDL_MapRGB(screen->format, 200, 200, 0);
+		int markerCol = SDL_MapRGB(screen->format, 10, 5, 25);
 
 		int startX = (int)(0-cameraX);
 		DrawRectangle(screen, startX, 80, 10, 260, markerCol, markerCol);
 		int endX = (int)(STAGE_W - cameraX);
 		DrawRectangle(screen, endX - 10, 80, 10, 260, markerCol, markerCol);
 
+		// floor
+		int floorScreenY = FLOOR_Y - (int)cameraY;
+		DrawRectangle(screen, 0, floorScreenY, SCREEN_WIDTH, FLOOR_H, floorEdge, floorCol);
+
 		// player (eti.bmp is placeholder sprite)
 		int px = (int)(playerX - cameraX);
-		int py = (int)(playerY - cameraY) - currentSprite->h / 2;
+		int py = (int)(playerY - cameraY - z) - currentSprite->h / 2;
 		DrawSurface(screen, currentSprite, px, py);
 
 		fpsTimer += delta;
@@ -370,6 +450,28 @@ int main(int argc, char **argv) {
 					if(event.key.keysym.sym == SDLK_ESCAPE) quit = 1;
 					else if(event.key.keysym.sym == SDLK_n) {
 						NewGame(stageTime, cameraX, cameraY, playerX, playerY);
+						inAttack = false; attackTimer = 0.0;
+						inJump = false; z = 0.0; vz = 0.0;
+					}
+					else if (event.key.repeat == 0) {
+						if (event.key.keysym.sym == SDLK_x) {
+							if (!inJump) {
+								inJump = true;
+								vz = jumpVel;
+							}
+						}
+						else if (event.key.keysym.sym == SDLK_z) {
+							if (action == ACT_NONE) {
+								action = ACT_LIGHT;
+								actionTimer = lightAttackDuration;
+							}
+						}
+						else if (event.key.keysym.sym == SDLK_y) {
+							if (action == ACT_NONE) {
+								action = ACT_HEAVY;
+								actionTimer = heavyAttackDuration;
+							}
+						}
 					}
 					break;
 				case SDL_KEYUP:
@@ -387,6 +489,9 @@ int main(int argc, char **argv) {
 	SDL_FreeSurface(sprStand);
 	SDL_FreeSurface(sprWalk1);
 	SDL_FreeSurface(sprWalk2);
+	SDL_FreeSurface(sprJump);
+	SDL_FreeSurface(sprAttackHeavy);
+	SDL_FreeSurface(sprAttackLight);
 	SDL_FreeSurface(charset);
 	SDL_FreeSurface(screen);
 	SDL_DestroyTexture(scrtex);
